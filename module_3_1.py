6d2a36ab12b811a1bf477b9acf7d8766d108196b
dmitrygor1975 <dmitry_gor@bk.ru>
2024-09-05 11:11:08 +0300

    задание счетчик функций

--- module_3_1.py
+++ module_3_1.py
@@ -1,5 +1,4 @@
-from http.cookiejar import uppercase_escaped_char
-from idlelib.debugobj_r import remote_object_tree_item
+# Задание Модуль 3_1 "Счетчик вызовов"
 
 calls  = 0
 
@@ -15,14 +14,27 @@ def string_info (string):
     count_calls()
     return cortage_1
 
-#def is_contains (string, list_to_search):
+def is_contains (string, list_to_search):
+    global calls
+    is_contains = False
+    string = string.lower()
+    list_to_search = '-'.join(list_to_search).lower()
+    #str_2 = list_to_search_1.lower()
+    #print(list_to_search, string)
 
-string = input("Введите строку:")
-print(string_info(string))
+    if string in list_to_search: # проверка на вхождение подстроки в строку
+        is_contains=True
+
+    count_calls()
+    return is_contains
+
+#string = input("Введите строку:")
+#print(string_info(string))
 
 print(string_info('Capybara'))
+print(string_info('Armageddon'))
 
-#print(string_info('Armageddon'))
-#print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-#print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN','Urbans'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
 print(calls)
