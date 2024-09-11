9e6daad38e668bc9eaa275802267628b6f98f7d0
dmitrygor1975 <dmitry_gor@bk.ru>
2024-09-11 21:12:49 +0300

    Add files via upload
    задание 3_3

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+ # Модуль 3.3 Распаковка позиционных параметров
+
+def print_params (a = 1, b = 'строка', c = True):
+    print (f"a={a} \n"
+           f"b={b} \n"
+           f"c={c} \n")
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [100, 'String', False]
+values_dict = {'a':111, 'b':222, 'c':333}
+
+print_params(*values_list)
+print_params(**values_dict)
