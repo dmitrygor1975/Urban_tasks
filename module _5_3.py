850726b31e35b7ed0e4302692d47d32a551de83a
dmitrygor1975 <dmitry_gor@bk.ru>
2024-10-01 10:38:01 +0300

    Module 5.3 Перегрузка операторов (вер2)
    с учетом проверки класса / типа

--- module _5_3.py
+++ module _5_3.py
@@ -23,34 +23,65 @@ class House:
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floor}"
 
     def __eq__(self, other):
-        return self.number_of_floor == other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor == other.number_of_floor
+
+        elif isinstance(other, int):
+            return self.number_of_floor == other
 
     def __gt__(self, other):
-        return self.number_of_floor > other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor > other.number_of_floor
+        elif isinstance(other, int):
+            return self.number_of_floor > other
 
     def __ge__(self, other):
-        return self.number_of_floor >= other.number_of_floor
+        if isinstance(other, int) or isinstance(other, House):
+            return self.number_of_floor >= other.number_of_floor
+        elif isinstance(other, int):
+            return self.number_of_floor >= other
 
     def __lt__(self, other):
-        return self.number_of_floor < other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor < other.number_of_floor
+        elif isinstance(other, int):
+            return self.number_of_floor < other
 
     def __le__(self, other):
-        return self.number_of_floor <= other.number_of_floor
+        if isinstance(other, House) :
+            return self.number_of_floor <= other.number_of_floor
+        elif isinstance(other, int):
+            return self.number_of_floor <= other
 
     def __ne__(self, other):
-        return self.number_of_floor != other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor != other.number_of_floor
+        elif isinstance(other, int):
+            return self.number_of_floor != other
 
-    def __add__(self, value):
-        self.number_of_floor += value
-        return self
+    def __add__(self, other):
+        if isinstance(other, House):
+            self.number_of_floor += other.number_of_floor
+            return self
+        elif isinstance(other, int):
+            self.number_of_floor += other
+            return self
 
-    def __iadd__(self, value):
-        self.number_of_floor += value
-        return self
+    def __iadd__(self, other):
+        if isinstance(other, House):
+            self.number_of_floor += other.number_of_floor
+            return self
+        elif isinstance(other, int):
+            self.number_of_floor += other
+            return self
 
-    def __radd__(self, value):
-        self.number_of_floor = value + self.number_of_floor
-        return self
+    def __radd__(self, other):
+        if isinstance(other, House):
+            self.number_of_floor = other.number_of_floor + self.number_of_floor
+            return self
+        elif isinstance(other, int):
+            self.number_of_floor = other + self.number_of_floor
+            return self
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -76,3 +107,13 @@ print(h1 >= h2) # __ge__
 print(h1 < h2) # __lt__
 print(h1 <= h2) # __le__
 print(h1 != h2) # __ne__
+
+print(h1 != 15.2) # __ne__
+
+h3=h1
+h4=h2
+
+print (h4+h3)
+print (h3+h4)
+
+print (h4+15.2) # так как второй аргумент н Int, то будет ошибка (None)
