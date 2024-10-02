06b450a837db0f6d383bc804dda9046197ac7d6e
dmitrygor1975 <dmitry_gor@bk.ru>
2024-10-02 14:25:06 +0300

    Задача История строительства
    вер 2

--- module _5_4.py
+++ module _5_4.py
@@ -4,12 +4,12 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])  # Добавляем новый объект в историю создания
         return super().__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floor = number_of_floors
-        House.houses_history.append(name) # Добавляем новый объект в историю создания
 
 
     def __del__(self):
@@ -17,16 +17,18 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
-print(h1)
+#print(h1)
 print(House.houses_history)
 
 h2 = House('ЖК Акация', 20)
-print(h2)
+#print(h2)
 print(House.houses_history)
 
 h3 = House('ЖК Матрёшки', 20)
-print(h3)
+#print(h3)
 print(House.houses_history)
 
-#del h2
-#del h3
+del h2
+del h3
+
+# в конце программы удаляет последний экземпляр  ЖК Эльбурус (происходит очистка памяти)
