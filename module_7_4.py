abf864e3e4b93895d7c0b7d62c5a90246831edef
dmitrygor1975 <dmitry_gor@bk.ru>
2024-10-23 22:44:41 +0300

    модуль 7_4
    задание "Форматирование строк"

---
+++ module_7_4.py
@@ -0,0 +1,47 @@
+# Домашнее задание по теме "Форматирование строк".
+
+team1_name ='Волшебники данных'
+team2_name = 'Мастера кода'
+
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+#team1_time = 1552.512
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+
+#challenge_result = 'Победа команды Волшебники данных!'
+
+# Использование %:
+
+print('В команде %s участников:%s!' % (team1_name,team1_num))
+print('В команде %s участников:%s!' % (team2_name,team2_num))
+print('Итого сегодня в командах участников:%s и %s!' % (team1_num,team2_num))
+
+
+#Использование format():
+print('Команда Волшебники данных решила задач: {}'.format(score_2))
+print('Волшебники данных решили задачи: {}'.format(team2_time))
+
+#Использование f-строк:
+
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    challenge_result = 'Победа команды Мастера кода!'
+    print(f'Результат битвы: {challenge_result}')
+
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    challenge_result = 'Победа команды Волшебники Данных!'
+    print(f'Результат битвы: {challenge_result}')
+
+else:
+    challenge_result = 'Ничья!'
+    print(f'Результат битвы: {challenge_result}')
+
+
+T=round(((team1_time+team2_time)/tasks_total),1)
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {T} секунды на задачу!')
