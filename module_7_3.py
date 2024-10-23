a37d21777581d454f7bcd68307521356de9ea693
dmitrygor1975 <dmitry_gor@bk.ru>
2024-10-23 20:35:43 +0300

    ДЗ оператор WITH
    модуль 7_3

--- module_7_3.py
+++ module_7_3.py
@@ -7,9 +7,9 @@ class WordsFinder:
 
     def get_all_words (self):
         all_words = {}
-        str_2=str()
-        for i in range (len(self.file_name)):
 
+        for i in range (len(self.file_name)):
+            str_2 = str()
             with open (self.file_name[i]) as file:
 
                 for line in file:
@@ -26,7 +26,30 @@ class WordsFinder:
 
         return all_words
 
+    def find (self, txt):
 
-finder_1 = WordsFinder ('test_file.txt')
+        dict_of_words = self.get_all_words()
+        result = {}
 
-print(finder_1.get_all_words())
+        for name, words in dict_of_words.items():
+            ind=words.index(txt.lower())+1
+            result.update({name: ind})
+        return result
+
+    def count(self, txt):
+
+        dict_of_words = self.get_all_words()
+        result = {}
+
+        for name, words in dict_of_words.items():
+            result.update({name:words.count(txt.lower())})
+        return result
+
+finder_1 = WordsFinder ('test_file.txt','test_file_2.txt')
+#finder_1 = WordsFinder ('test_file.txt')
+#finder_1 = WordsFinder ('test_file_2.txt')
+
+
+print(finder_1.get_all_words()) # Все слова
+print(finder_1.find('TEXT'))    # 3 слово по счёту
+print(finder_1.count('teXT'))   # 4 слова teXT в тексте всего
