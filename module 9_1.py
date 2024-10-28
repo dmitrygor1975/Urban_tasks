8a78dcac8d08f311730c044a4e86f76171212560
dmitrygor1975 <dmitry_gor@bk.ru>
2024-10-28 23:10:18 +0300

    Module 9_1
    Домашнее задание "Вызов разом"

---
+++ module 9_1.py
@@ -0,0 +1,10 @@
+#Задача "Вызов разом":
+
+def apply_all_func(int_list, *functions):
+    result = {}
+    for func in functions:
+        result.update({func.__name__:func(int_list)})
+    return result
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
