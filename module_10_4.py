20a04f6b1f64d189e9d435d8083ee5a4b1c26c03
dmitrygor1975 <dmitry_gor@bk.ru>
2024-12-16 19:40:07 +0300

    модуль 10.4
     Очереди для обмена данными между потоками (вер_2)

--- module_10_4.py
+++ module_10_4.py
@@ -24,16 +24,18 @@ class Guest (threading.Thread):
         print(f'поток запущен {self.name} ')
         time_delay = random.randint(3, 10)
         sleep(time_delay)
+        #print(f'текущий поток Гостя {threading.current_thread()}')
         #print(f'текущий поток Гостя {threading.current_thread().is_alive}')
         #print(f'текущий поток Гостя в RUN = {threading.current_thread().name} жив или нет:  {self.is_alive()}')
-
+        #print(f'перечень запущенных потоков внутри Run {threading.enumerate()}')
+        #print(f' количестов живых потоков внутри Run  {threading.active_count()}')
 
 class Cafe:
     def __init__(self, *tables):
         self.queue = Queue()
         self.tables = tables
 
-    def guest_arrival (self,*geusts):
+    def guest_arrival (self,*guests):
         global free_tables
         free_tables = len(tables)
 
@@ -43,6 +45,7 @@ class Cafe:
                     if self.tables[i].guest is None: # стол не занят
                         self.tables[i].guest = guest # посадили Гостя за свободный по порядку стол
                         print (f'{self.tables[i].guest.name} сел(-а) за стол номер {self.tables[i].number}' )
+                        #print(f'текущий поток в Arrival {threading.current_thread()}')
                         guest.start() #
                         guest.join()
                         free_tables -= 1
@@ -52,27 +55,41 @@ class Cafe:
                 cafe.queue.put(guest)
                 print(f'{guest.name} в очереди') #=.put(guest)
 
-        return (free_tables)
+        return (print('все гости прибыли'))
 
     def discuss_guests (self):
         #free_tables=0
-        while not self.queue.empty() or free_tables<len(tables):
+        #while not self.queue.empty() or free_tables<len(tables):
+
+        while not self.queue.empty():
             for i in range(len(tables)):
-                if self.tables[i].guest is not None:
+
+                if self.tables[i].guest is not None and not self.tables[i].guest.is_alive():
                     print(f'Гость {self.tables[i].guest.name} покушал (а) и ушел (ушла)')
                     print(f'Стол номер {self.tables[i].number} свободен')
                     self.tables[i].guest = None #освободили текущий стол
-                    #free_tables += 1
-                    self.tables[i].guest = cafe.queue.get(timeout=1) # посадили Гостя из очереди за освободившийся стол
+
+                if not self.queue.empty() and self.tables[i].guest is None:
+                    self.tables[i].guest = cafe.queue.get() # посадили Гостя из очереди за освободившийся стол
                     print(f'{self.tables[i].guest.name} вышел(-ла) из очереди '
-                          f'и сел(-а) за стол номер {self.tables[i].number}>')
+                          f'и сел(-а) за стол номер {self.tables[i].number}>'
+                          f'')
                     self.tables[i].guest.start() #запустили новый поток Гостя
+
+        # если очередь пустая, то завершаем обслуживание занятых столов
+        else:
+            for i in range(len(tables)):
+                if self.tables[i].guest is not None and not self.tables[i].guest.is_alive():
+                    #print(f'поток гостя {self.tables[i].guest.is_alive()}')
+                    print(f'Гость {self.tables[i].guest.name} покушал (а) и ушел (ушла)')
+                    print(f'Стол номер {self.tables[i].number} свободен')
+                    self.tables[i].guest = None  # освободили текущий стол
 
 
 
 # Создание столов
 tables = [Table (number) for number in range(1, 6)]
-print(f'Создание столов {tables}')
+#print(f'Создание столов {tables}')
 
 # Имена гостей
 guests_names = [
@@ -85,13 +102,15 @@ guests_names = [
 guests = [Guest(name) for name in guests_names]
 print (f'Создание гостей {guests}')
 
-
 # Заполнение кафе столами
 cafe = Cafe(*tables)
 print (f'создали Cafe {Cafe}')
 
 # Приём гостей
 cafe.guest_arrival(*guests)
+#print(f'перечень запущенных потоков {threading.enumerate()}')
+#print(f'количестов потоков {threading.active_count()}')
+#print(guests[0].is_alive)''
 
 # Обслуживание гостей
 cafe.discuss_guests()
